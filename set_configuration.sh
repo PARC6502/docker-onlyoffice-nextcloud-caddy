901f570efe1019d1bf1d3fcb97f5befe35973950
parc6502 <ibrahim@serafy.co.uk>
2019-04-19 18:09:06 +0100

    setting correct config

--- set_configuration.sh
+++ set_configuration.sh
@@ -6,13 +6,13 @@ docker exec -u www-data app-server php occ --no-warnings config:system:get trust
 
 if ! grep -q "app-server" trusted_domain.tmp; then
     TRUSTED_INDEX=$(cat trusted_domain.tmp | wc -l);
-    docker exec -u www-data app-server php occ --no-warnings config:system:set trusted_domains $TRUSTED_INDEX --value="nginx-server"
+    docker exec -u www-data app-server php occ --no-warnings config:system:set trusted_domains $TRUSTED_INDEX --value="caddy-server"
 fi
 
 rm trusted_domain.tmp
 
 docker exec -u www-data app-server php occ --no-warnings app:install onlyoffice
 
-docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
+docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/docs/"
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-document-server/"
-docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nginx-server/"
+docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://caddy/"
